9e284c79b3e754e94f22212c4f762e9f1d464f02
Damir Mustafin <frenchthebread@gmail.com>
2015-09-14 15:33:57 +0900

    initial commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Damir Mustafin on 14/09/15.
+//  Copyright (c) 2015 Damir Mustafin. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hello World!\n";
+    
+    return 0;
+}
