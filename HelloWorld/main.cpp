051c91bf492f0aa9dbebd758da41d4a3010bec06
Damir Mustafin <frenchthebread@gmail.com>
2015-09-25 10:22:50 +0900

    simple class for dates

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,17 +7,60 @@
 //
 
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
 
 
-int main(int argc, const char * argv[]) {
+class DayOfYear{
+public:
+    void output();
+    int month;
+    int day;
+};
+
+int main() {
     
-    for (int i=0; i<=30; i++) {
-        cout << i << " - " << rand()%100 << endl;
-    }
-  
+    DayOfYear today, birthday;
+    
+    cout << "Today's date \n";
+    
+    cout << "Enter today's month:\n";
+    cin >> today.month;
+    cout << "Enter today's day of the month:\n";
+    cin >> today.day;
+    
+    cout << "You birthday's date \n";
+    
+    cout << "Enter month:\n";
+    cin >> birthday.month;
+    cout << "Enter day of the month:\n";
+    cin >> birthday.day;
+    
+    cout << "Today's date is : ";
+    today.output();
+    cout << "\n";
+    cout << "You birthday is : ";
+    birthday.output();
+    
+    
     return 0;
+}
+
+void DayOfYear::output(){
+    switch(month){
+        case 1: cout << "January "; break;
+        case 2: cout << "February "; break;
+        case 3: cout << "March "; break;
+        case 4: cout << "April "; break;
+        case 5: cout << "May "; break;
+        case 6: cout << "June "; break;
+        case 7: cout << "July "; break;
+        case 8: cout << "August "; break;
+        case 9: cout << "September "; break;
+        case 10: cout << "October "; break;
+        case 11: cout << "November "; break;
+        case 12: cout << "December "; break;
+        default: cout << "Error";
+    }
+    
+    cout << day;
 }
