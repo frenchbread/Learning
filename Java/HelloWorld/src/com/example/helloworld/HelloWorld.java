4c54dc2b78ac707139c2345d5630f245dc787da5
Damir Mustafin <frenchthebread@gmail.com>
2015-10-08 16:19:28 +0900

    java hello world

---
+++ Java/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/**
+ * Created by damir on 08/10/15.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
