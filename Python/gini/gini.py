3ea1a114e4fa3986421f75525cdbd60f5115792e
Damir Mustafin <frenchthebread@gmail.com>
2015-10-19 02:23:35 +0900

    impr

--- Python/gini/gini.py
+++ Python/gini/gini.py
@@ -1,9 +1,9 @@
 
 sample = [1,1,1,1,1,2,2,2,3,3,3,3,3,4,4]
+sample21 = [1,1,1,1,2,2,2,3]
+sample22 = [1,3,3,3,3,4,4]
 
-sample1 = [1,1,1,1,1,2,2,2,3,3,3,3,3,4,4]
-sample2 = [1,1,1,1,2,2,2,3]
-sample25 = [1,3,3,3,3,4,4]
+import numpy as np
 
 def gini(list):
   sorted_list = sorted(list)
@@ -14,5 +14,20 @@ def gini(list):
   fair_area = height * len(list) / 2
   return (fair_area - area) / fair_area
 
-# print gini(sample
-print 1 - gini(sample2) * gini(sample2) - gini(sample25) * gini(sample25)
+
+def gini_coeff(x):
+    # requires all values in x to be zero or positive numbers,
+    # otherwise results are undefined
+    n = len(x)
+    s = x.sum()
+    r = np.argsort(np.argsort(-x)) # calculates zero-based ranks
+    return 1 - (2.0 * (r*x).sum() + s)/(n*s)
+
+sample = np.asarray(sample)
+
+gB = gini(sample)
+gA1 = gini(sample21)
+gA2 = gini(sample22)
+
+print gB
+print gB - gA1 * 8/15 - gA2 * 7/15
