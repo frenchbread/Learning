70533790886f53a1c0ae24e8c1baae421d5ea866
Damir Mustafin <frenchthebread@gmail.com>
2015-10-18 21:32:29 +0900

    fix

--- Python/gini/gini.py
+++ Python/gini/gini.py
@@ -1,5 +1,9 @@
 
-sample = [1,1,1,1,1,2,2,2,3,3,3,3,3,4,4];
+sample = [1,1,1,1,1,2,2,2,3,3,3,3,3,4,4]
+
+sample1 = [1,1,1,1,1,2,2,2,3,3,3,3,3,4,4]
+sample2 = [1,1,1,1,2,2,2,3]
+sample25 = [1,3,3,3,3,4,4]
 
 def gini(list):
   sorted_list = sorted(list)
@@ -10,4 +14,5 @@ def gini(list):
   fair_area = height * len(list) / 2
   return (fair_area - area) / fair_area
 
-print gini(sample)
+# print gini(sample
+print 1 - gini(sample2) * gini(sample2) - gini(sample25) * gini(sample25)
