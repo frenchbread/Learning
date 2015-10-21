f315f469539cc87113a75b3663a00db8a93597de
Damir Mustafin <frenchthebread@gmail.com>
2015-10-21 02:33:39 -0700

    Unix class code added

---
+++ C/ClassTask/Task2/task2.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main(void){
+	char buf[512];
+	int n;
+	symlink("unix.txt", "unix.sym");
+	n = readlink("unix.sym", buf, 512);
+	
+	if (n == -1) {
+		perror("readlink");
+		return 0;
+	}
+	
+	buf[n] = '\0';
+	printf("unix.sym : Readlink = %s\n", buf);
+	
+}
