ed072407b193ee3bc85e295d50dcb190d953216a
Damir Mustafin <frenchthebread@gmail.com>
2015-11-05 14:22:51 +0900

    Inheritance of private members by indirect way

---
+++ Java/Inheritance/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SavingAccount sAcc = new SavingAccount(1000);
+
+        sAcc.saveMoney(1000);
+        sAcc.saveMoney(1000);
+        sAcc.showSavedMoney();
+
+    }
+}
+
+class Accumulator{
+    private int val;
+
+    Accumulator (int init){
+        val = init;
+    }
+    protected void accummulate(int num) {
+        if (num<0) return;
+        val += num;
+    }
+    protected int getAccVal(){
+        return val;
+    }
+}
+
+class SavingAccount extends Accumulator{
+    public SavingAccount(int initDep){
+        super(initDep);
+    }
+    public void saveMoney(int money){
+        accummulate(money);
+    }
+    public void showSavedMoney(){
+        System.out.println("Accumulated money: " + getAccVal());
+    }
+}
