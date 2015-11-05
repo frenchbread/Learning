f9cec3f32f0bafa95b8fc59db416905687f8665f
Damir Mustafin <frenchthebread@gmail.com>
2015-11-05 13:57:57 +0900

    businessman class

--- Java/Basic/src/Main.java
+++ Java/Basic/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
         System.out.println("Hello World!");
 
-        Man man = new Man("Damir");
+        BusinessMan man = new BusinessMan("Damir", "Sampo Software", "JavaScript developer");
 
-        man.tellYourName();
+        man.tellYourInfo();
 
 
     }
@@ -17,5 +17,20 @@ class Man {
     public Man(String name){ this.name = name; }
     public void tellYourName(){
         System.out.println("My name is " + name);
+    }
+}
+
+class BusinessMan extends Man {
+    String company;
+    String position;
+    public BusinessMan(String name, String company, String position){
+        super(name);
+        this.company = company;
+        this.position = position;
+    }
+    public void tellYourInfo(){
+        System.out.println("I work in " + company);
+        System.out.println("My position is " + position);
+        tellYourName();
     }
 }
