853b9806cda75595eeb3b5beed94265fa95f870a
Damir Mustafin <frenchthebread@gmail.com>
2015-11-11 08:53:24 -0800

    hw4 added

--- C/HW4/fork.cpp
+++ C/HW4/fork.cpp
@@ -1,5 +1,5 @@
 #include "../apue.h"
-#inclide "../error.c"
+#include "../error.c"
 
 int glob = 6;
 char buf[] = "a write to stdout\n";
@@ -7,13 +7,13 @@ char buf[] = "a write to stdout\n";
 int main (void){
 	int var;
 	pid_t pid;
-	
+
 	var = 88;
-	
-	if (write(STDOUT_FILENO, buf, sizeof(buf)-1)) != sizeof(buf)-1)
+
+	if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1)
 		err_sys("write error");
 	printf("before fork\n");
-	
+
 	if ((pid = fork()) < 0){
 		err_sys("forl error");
 	}else if(pid == 0){
@@ -21,8 +21,8 @@ int main (void){
 	}else{
 		sleep(2);
 	}
-	
+
 	printf("pid = %d, glob = %d, var %d\n", getpid(), glob, var);
-	
+
 	exit(0);
 }
