439c630e57a9d03c0a4dbbed03b837c88acd8b40
Damir Mustafin <frenchthebread@gmail.com>
2015-11-11 23:26:01 +0900

    dark

---
+++ Cpp/dark/dc/dc/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  dc
+//
+//  Created by Damir Mustafin on 11/11/15.
+//  Copyright (c) 2015 Damir Mustafin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    int x = 10;
+    
+    std::cout << "Hello, World!\n";
+    
+    while (x --> 0){
+        std::cout << x << std::endl;
+    }
+    
+    
+    return 0;
+}
