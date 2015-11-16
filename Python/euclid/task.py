a5ec20f079a91bcf3138fa14365d3d714926a47b
Damir Mustafin <frenchthebread@gmail.com>
2015-11-16 13:51:21 +0900

    dist

--- Python/euclid/task.py
+++ Python/euclid/task.py
@@ -1,3 +1,5 @@
+from scipy.spatial import distance
+
 file = 'iris.txt'
 
 f = open(file)
@@ -5,4 +7,16 @@ data = []
 
 for line in f:
 	split_line=line.split()
-	print split_line
+	new_line=[float(x) for x in split_line[:-1]]
+	new_line=[float(x) for x in split_line[:-2]]
+	data.append(new_line)
+f.close()
+	
+n1 = data[0] 
+n51 = data[50]
+n101 = data[100]
+n150 = data[149]
+
+dst = distance.euclidean(n1, n150)
+
+print dst
