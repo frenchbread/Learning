bbdcb9ef7388df91895213d906422565594637cb
Damir Mustafin <frenchthebread@gmail.com>
2016-04-04 12:00:24 +0300

    Fix folder structure

--- 30-hr/1/1.cpp
+++ 30-hr/1/1.cpp
@@ -1,18 +1,26 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <iomanip>
+#include <limits>
+
 using namespace std;
 
 int main() {
-   string inputString; // declare a variable to hold our input
-   getline(cin, inputString); // get a line of input from cin and save it to our variable
-  
-   // Your first line of output goes here
-   cout << "Hello, World." << endl;
-
-   // Write the second line of output
-
-   return 0;
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    // Declare second integer, double, and String variables.
+    int a; double b; string c1, c2;
+    // Read and save an integer, double, and String to your variables.
+    cin >> a;
+    cin >> b;
+    getline(cin, c1);
+    getline(cin, c2);
+    // Print the sum of both integer variables on a new line.
+    cout << i + a << endl;
+    // Print the sum of the double variables on a new line.
+    cout << setprecision(1) << std::fixed << d + b << endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s + c2 << endl;
+    return 0;
 }
