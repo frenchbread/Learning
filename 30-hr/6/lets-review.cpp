da9b9731b3d86d35a4c6c68e1318b9c662aa1576
Damir Mustafin <frenchthebread@gmail.com>
2016-04-08 14:17:51 +0300

    cpp - day 6 - lets-review

---
+++ 30-hr/6/lets-review.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int testCases, k, m;
+    bool correct;
+    string s, rs;
+    char str[50];
+    cin >> testCases;
+    for (int i = 0; i < testCases; i++) {
+        cin >> str;
+        correct = true;
+        for (k=1, m=strlen(str)-1;k<strlen(str); k++, m--) {
+            if (abs(str[k] - str[k-1]) != abs(str[m]-str[m-1])) {
+                correct = false;
+                break;
+            }
+        }
+        if (correct) {
+            cout << "Funny" << endl;
+        } else {
+            cout << "Not Funny" << endl;
+        }
+    }
+    return 0;
+}
