7923664440af0af5b4b5550f98d1e7596c9a908f
Damir Mustafin <frenchthebread@gmail.com>
2016-04-08 14:22:33 +0300

    cpp - day 6 -additional check

--- 30-hr/6/lets-review.cpp
+++ 30-hr/6/lets-review.cpp
@@ -7,26 +7,29 @@
 using namespace std;
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int testCases, k, m;
     bool correct;
     string s, rs;
-    char str[50];
+    char str[10000];
     cin >> testCases;
-    for (int i = 0; i < testCases; i++) {
-        cin >> str;
-        correct = true;
-        for (k=1, m=strlen(str)-1;k<strlen(str); k++, m--) {
-            if (abs(str[k] - str[k-1]) != abs(str[m]-str[m-1])) {
-                correct = false;
-                break;
+    if ((testCases > 0) && (testCases <=10)) {
+        for (int i = 0; i < testCases; i++) {
+            cin >> str;
+            correct = true;
+            for (k=1, m=strlen(str)-1;k<strlen(str); k++, m--) {
+                if (abs(str[k] - str[k-1]) != abs(str[m]-str[m-1])) {
+                    correct = false;
+                    break;
+                }
+            }
+            if (correct) {
+                cout << "Funny" << endl;
+            } else {
+                cout << "Not Funny" << endl;
             }
         }
-        if (correct) {
-            cout << "Funny" << endl;
-        } else {
-            cout << "Not Funny" << endl;
-        }
     }
+
     return 0;
 }
