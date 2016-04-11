6237c0e609fdce00bb751d11db30d12a81fb1dac
Damir Mustafin <frenchthebread@gmail.com>
2016-04-11 19:05:04 +0300

    cpp - day 7 - arrays

---
+++ 30-hr/7/arrays.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    for (int j = n-1; j>=0; j--) {
+        cout << arr[j] << " ";
+    }
+    return 0;
+}
