e37e5debb53b9be101d00068145647c5977ef6f0
Damir Mustafin <frenchthebread@gmail.com>
2016-08-23 23:21:29 +0300

    duplicate, tail_duplicate

--- erlang/code/recursive.erl
+++ erlang/code/recursive.erl
@@ -4,7 +4,9 @@
   fac/1,
   tail_fac/1,
   len/1,
-  tail_len/1
+  tail_len/1,
+  duplicate/2,
+  tail_duplicate/2
 ]).
 
 %% basic fuctorial function
@@ -30,3 +32,20 @@ tail_len(L) -> tail_len(L,0).
 
 tail_len([], Acc) -> Acc;
 tail_len([_|T], Acc) -> tail_len(T, Acc+1).
+
+%% duplicate
+
+duplicate(0,_) ->
+  [];
+duplicate(N,Term) when N > 0 ->
+  [Term|duplicate(N-1,Term)].
+
+%% tail duplicate
+
+tail_duplicate(N,Term) ->
+  tail_duplicate(N-1,Term,[]).
+
+tail_duplicate(0,_,List) ->
+  List;
+tail_duplicate(N,Term,List) when N > 0 ->
+  tail_duplicate(N-1, Term, [Term|List]).
