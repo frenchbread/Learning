2003a98c18218647b5fb43af2c29cd4235b7cbc8
Damir Mustafin <frenchthebread@gmail.com>
2016-08-23 15:06:28 +0300

    Length of a list

--- erlang/code/recurcive.erl
+++
@@ -1,9 +0,0 @@
--module(recursive).
-
--export([fac/1]).
-
-
-%% basic fuctorial function
-
-fac(0) -> 1;
-fac(N) when N > 0 -> N*fac(N-1).
--- erlang/code/recursive.erl
+++ erlang/code/recursive.erl
@@ -1,6 +1,10 @@
 -module(recursive).
 
--export([fac/1,tail_fac/1]).
+-export([
+  fac/1,
+  tail_fac/1,
+  len/1
+]).
 
 %% basic fuctorial function
 
@@ -13,3 +17,8 @@ tail_fac(N) -> tail_fac(N,1).
 
 tail_fac(0,Acc) -> Acc;
 tail_fac(N,Acc) when N > 0 -> tail_fac(N-1, N*Acc).
+
+%% length of a list
+
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
