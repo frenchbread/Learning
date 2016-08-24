7a59838312ea86f10878c1cac50926ca4e7554d3
Damir Mustafin <frenchthebread@gmail.com>
2016-08-24 21:38:35 +0300

    quicksort

--- erlang/code/recursive.erl
+++ erlang/code/recursive.erl
@@ -14,7 +14,9 @@
   zip/2,
   lenient_zip/2,
   tail_zip/2,
-  tail_lenient_zip/2
+  tail_lenient_zip/2,
+  quicksort/1,
+  lc_quicksort/1
 ]).
 
 %% basic fuctorial function
@@ -112,3 +114,25 @@ tail_lenient_zip([],_,Acc) -> Acc;
 tail_lenient_zip(_,[],Acc) -> Acc;
 tail_lenient_zip([X|Xs],[Y|Ys], Acc) ->
   tail_lenient_zip(Xs,Ys,[{X,Y}|Acc]).
+
+%% sorting
+
+%% quicksort
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  { Smaller, Larger } = partition(Pivot,Rest,[],[]),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition(_,[],Smaller,Larger) -> { Smaller, Larger };
+partition(Pivot,[H|T],Smaller,Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+    H > Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.
+
+%% list comprehensions quicksort
+
+lc_quicksort([]) -> [];
+lc_quicksort([Pivot|Rest]) ->
+    lc_quicksort([Smaller || Smaller <- Rest, Smaller =< Pivot])
+    ++ [Pivot] ++
+    lc_quicksort([Larger || Larger <- Rest, Larger > Pivot]).
