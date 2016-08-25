4043d25efb4f87900745e1c89d109a62f119a12c
Damir Mustafin <frenchthebread@gmail.com>
2016-08-25 16:03:31 +0300

    hhfuns

---
+++ erlang/code/hhfuns.erl
@@ -0,0 +1,36 @@
+-module(hhfuns).
+
+-compile(export_all).
+
+one() -> 1.
+two() -> 2.
+
+add(X,Y) -> X() + Y().
+
+increment([]) -> [];
+increment([H|T]) -> [H+1|increment(T)].
+
+decrement([]) -> [];
+decrement([H|T]) -> [H-1|decrement(T)].
+
+map(_, []) -> [];
+map(F, [H|T]) -> [F(H)|map(F,T)].
+
+incr(X) -> X+1.
+decr(X) -> X-1.
+
+% base(A) ->
+%   B = A + 1,
+%   F = fun() -> A * B end,
+%   F().
+
+base() ->
+  A = 1,
+  (fun(A) -> A = 2 end)(2).
+
+a() ->
+  Secret = "pony",
+  fun() -> Secret end.
+
+b(F) ->
+  "a/0's password is " ++ F().
