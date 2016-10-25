86cbf9f1ea93cf325f1350c717e9a81992704039
Damir Mustafin <frenchthebread@gmail.com>
2016-10-26 02:40:41 +0300

    Reminder init

---
+++ erlang/code/reminder/src/event.erl
@@ -0,0 +1,25 @@
+-module(event).
+
+-export([loop/1]).
+
+-record(state, {server,
+                name="",
+                to_go=0}).
+
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
+  receive
+    {Server, Ref, cancel} ->
+      Server ! {Ref, ok}
+  after T*1000 ->
+    if Next =:= [] ->
+        Server ! {done, S#state.name};
+      Next =/= [] ->
+        loop(S#state{to_go=Next})
+    end
+  end.
+
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used.
+normalize(N) ->
+  Limit = 49*24*60*60,
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
