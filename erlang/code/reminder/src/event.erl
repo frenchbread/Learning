8bfb6f4fe863c04221c0e55f5b52783affda3cde
Damir Mustafin <frenchthebread@gmail.com>
2016-11-19 16:38:48 +0200

    Revert "deps"
    This reverts commit 81c94d9633b0a113f54820d0023ac72f00090dcc.

--- erlang/code/reminder/src/event.erl
+++ erlang/code/reminder/src/event.erl
@@ -1,6 +1,6 @@
 -module(event).
 
--compile(export_all).
+-export([loop/1]).
 
 -record(state, {server,
                 name="",
@@ -23,36 +23,3 @@ loop(S = #state{server=Server, to_go=[T|Next]}) ->
 normalize(N) ->
   Limit = 49*24*60*60,
   [N rem Limit | lists:duplicate(N div Limit, Limit)].
-
-start(EventName, Delay) ->
-  spawn(?MODULE, init, [self(), EventName, Delay]).
-
-start_link(EventName, Delay) ->
-  spawn_link(?MODULE, init, [self(), EventName, Delay]).
-
-%%% event's innards
-init(Server, EventName, DateTime) ->
-  loop(#state{server=Server,
-              name=EventName,
-              to_go=time_to_go(DateTime)}).
-
-cancel(Pid) ->
-  %% Monitor if process is already dead
-  Ref = erlang:monitor(process, Pid),
-  Pid ! {self(), Ref, cancel},
-  receive
-    {Ref, ok} ->
-      erlang:demonitor(Ref, [flush]),
-      ok;
-    {'DOWN', Ref, process, Pid, _Reason} ->
-      ok
-  end.
-
-time_to_go(TimeOut={{_,_,_},{_,_,_}}) ->
-  Now = calendar:local_time(),
-  ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) -
-         calendar:datetime_to_gregorian_seconds(Now),
-  Secs = if ToGo > 0 -> ToGo,
-            ToGo =< 0 -> 0
-  end,
-  normalize(Secs).
